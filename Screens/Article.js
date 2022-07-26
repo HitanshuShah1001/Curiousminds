bd2c508a53ace7c3e69eae68129c09993f1eed5f
HitanshuShah1001 <hitanshushah5@gmail.com>
2022-07-26 19:49:10 +0530

    feat;Added play and pause functionality

--- Screens/Article.js
+++ Screens/Article.js
@@ -7,6 +7,8 @@ import {
   ScrollView,
   Button,
   Share,
+  Pressable,
+  Image
 } from "react-native";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -16,7 +18,7 @@ import { TextSizes } from "../TextContext.js";
 export default function Article() {
   const { titleSize, authortextSize, articletextSize,articleColor,pagecolor } =
     React.useContext(TextSizes);
-
+  const [imagesource,setImagesource] = useState(require('../images/pause.png'))
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [article, setArticle] = useState("");
@@ -24,9 +26,14 @@ export default function Article() {
   console.log(titleSize, authortextSize, articletextSize);
   useEffect(() => {
     getanother();
+    Speech.getAvailableVoicesAsync().then(res => {
+      console.log(res)
+    })
   }, []);
 
   const Sharearticle = () => {
+    Speech.pause()
+    setImagesource(require('../images/play-button.png'))
     Share.share({
       message: article.toString(),
     })
@@ -36,20 +43,30 @@ export default function Article() {
 
   const Speak = () => {
     SettoSpeak(true);
-    
     const thingtoSay = article;
-    Speech.speak(thingtoSay);
+    Speech.speak(thingtoSay,{language:"ar-SA"});
     Speech.isSpeakingAsync().then(res => {
       console.log(res)
     })
   };
 
 
-  const Pausespeech = () => {
-    Speech.pause()
+
+  const CheckStatus = () => {
+    if(imagesource===require('../images/pause.png')){
+      setImagesource(require('../images/play-button.png'))
+      Speech.pause()
+    }
+    else{
+      setImagesource(require('../images/pause.png'))
+      Speech.resume()
+    }
   }
 
   const getanother = () => {
+    Speech.stop()
+    setImagesource(require('../images/pause.png'))
+    SettoSpeak(false);
     axios
       .get("https://curiousmindsbackend.herokuapp.com/article")
       .then((res) => {
@@ -59,6 +76,11 @@ export default function Article() {
       });
   };
 
+  const StopAudio = () => {
+    Speech.stop()
+    SettoSpeak(false);
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: pagecolor }}>
       <View
@@ -67,13 +89,22 @@ export default function Article() {
           flexDirection: "row",
           justifyContent: "space-between",
           paddingHorizontal: 10,
+          alignItems:'center'
         }}
       >
         <Button title="Listen " onPress={Speak} />
         <Button title={`Read another`} onPress={getanother} />
         <Button title="Share " onPress={Sharearticle} />
       </View>
-      {tospeak && <View style={{ flexDirection: "row", flex: 0.3 }}><Button title="Pause" onPress={Pausespeech} /></View>}
+      {tospeak && <View style={{ flexDirection: "row", flex: 0.1,paddingHorizontal:15,flexDirection:'row',justifyContent:'space-between',alignItems:'center' }}>
+        {/* <Button title="Pause" onPress={Pausespeech} /> */}
+        <Pressable onPress={CheckStatus}>
+          <Image source={imagesource} style={{height:25,width:25}} />
+        </Pressable>
+        <Pressable onPress={StopAudio}>
+          <Image source={require('../images/stop-button.png')} style={{height:35,width:35}} />
+        </Pressable>
+        </View>}
       {article == "" && <ActivityIndicator size={"large"} />}
       {article != "" && (
         <ScrollView style={{ flex: 1.5 }}>
